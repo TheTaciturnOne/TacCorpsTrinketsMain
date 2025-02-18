b300efe346d64ea347125da795aa4234b78d6f3b
n1tr0n__ <166323600+n1tr0nR@users.noreply.github.com>
2025-02-18 01:53:43 +0000

    Another one....
    - Made it so Quartzite Hammer and Tac Plushie entities can be seen from far instead of poofing away
    (I know i know... TWO COMMITS??? Im carrying /j)

--- src/main/java/com/thetaciturnone/taccorpstrinkets/registries/TacEntities.java
+++ src/main/java/com/thetaciturnone/taccorpstrinkets/registries/TacEntities.java
@@ -24,6 +24,6 @@ public class TacEntities {
 	}
 
 	private static <T extends Entity> EntityType<T> createEntityType(EntityType.EntityFactory<T> factory) {
-		return FabricEntityTypeBuilder.create(SpawnGroup.MISC, factory).dimensions(EntityDimensions.changing(0.5f, 0.5f)).trackRangeBlocks(4).trackedUpdateRate(20).build();
+		return FabricEntityTypeBuilder.create(SpawnGroup.MISC, factory).dimensions(EntityDimensions.changing(0.5f, 0.5f)).trackRangeBlocks(64).trackedUpdateRate(20).build();
 	}
 }
